387e5ffa27818c2cc96c883bcef09ab16631cd59
TanyaLark <54994530+TanyaLark@users.noreply.github.com>
2020-10-04 15:29:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//1, 5, 20, 50
+// let sum = +prompt('Введите сумму ');
+// console.log('Сумма ', sum, ' грн.');
+
+// let k50 = Math.floor(sum / 50); //70,36
+// console.log('UAH 50 x', k50, ' шт.');
+// sum -= (50 * k50);
+
+// let k20 = Math.floor(sum / 20); //70,36
+// console.log('UAH 20 x', k20, ' шт.');
+// sum -= (20 * k20);
+
+// let k5 = Math.floor(sum / 5); //70,36
+// console.log('UAH 5 x', k5, ' шт.');
+// sum -= (5 * k5);
+
+// sum -= (1 * k1);
+// console.log('UAH 1 x', sum, ' шт.');
+
+let name = 'Ivan'
+
+let welcome =  `hello ${name} !!!`;//шаблонизация
+
+console.log (welcome);
+
+
+
+
+// let sum = 17;
+// if (sum > 17){
+//     console.log('nado v nalogovuyu')
+// } else{ console.log('Не переживай')}
